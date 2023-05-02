b9bc156034e92221cc05d362db89d5950e60bfc3
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-05-02 05:15:30 +0100

    adding gateway and optimizing some configurations

---
+++ category-service/src/test/java/com/wecouldcode/deliverymanagement/deliverypersonservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecouldcode.deliverymanagement.deliverypersonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
